e11941778037e2da78a36e4e8dc960f3a0af9f56
mairaaraujo <maiira.araujo@gmail.com>
2017-06-08 20:03:01 -0300

    improved tests

--- src/main/java/com/twu/biblioteca/BibliotecaApp.java
+++ src/main/java/com/twu/biblioteca/BibliotecaApp.java
@@ -5,18 +5,30 @@ import com.twu.util.InputAsker;
 
 public class BibliotecaApp {
 
-    public  void startApp() {
-       printWelcomeMessage();
-       //continueToMenu(new InputAsker(System.in, System.out));
+
+
+
+    public void startApp() {
+      printMessage(BibliotecaConstants.WELCOME_MESSAGE);
 
     }
 
-    public void printWelcomeMessage() {
+    public void printMessage(String message) {
 
-        System.out.println(BibliotecaConstants.WELCOME_MESSAGE);
+        System.out.println(message);
     }
 
     public void continueToMenu(InputAsker inputAsker) {
+        inputAsker.ask(BibliotecaConstants.CONTINUE_MESSAGE);
+        inputAsker.pressToContinue();
 
+        showMenu();
+    }
+
+
+    public void showMenu() {
+
+        printMessage(BibliotecaConstants.MENU);
+        printMessage(BibliotecaConstants.MENU_OPTIONS);
     }
 }
--- src/test/java/com.twu/biblioteca/BibliotecaTest.java
+++ src/test/java/com.twu/biblioteca/BibliotecaTest.java
@@ -3,28 +3,45 @@ package com.twu.biblioteca;
 
 import com.twu.util.InputAsker;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class BibliotecaTest {
 
-    BibliotecaApp bibliotecaApp = Mockito.mock(BibliotecaApp.class);
+    @Mock
+    BibliotecaApp bibliotecaApp;
 
+    @Mock
+    InputAsker inputAsker;
 
     @Test
     public void checkWelcomeMessageIsBeingInvoked() throws Exception {
 
+        Mockito.doCallRealMethod().when(bibliotecaApp).startApp();
         bibliotecaApp.startApp();
-        Mockito.verify(bibliotecaApp).printWelcomeMessage();
+        Mockito.verify(bibliotecaApp).printMessage(Mockito.anyString());
     }
 
     @Test
-    public void checkContinueToMenuIsBeingInvoked(){
+    public void printMessageTest() throws Exception {
 
-        InputAsker inputAsker = new InputAsker(System.in, System.out);
+        String message = "teste";
 
-        bibliotecaApp.startApp();
-        Mockito.verify(bibliotecaApp).continueToMenu(inputAsker);
+        bibliotecaApp.printMessage(message);
+        Mockito.verify(bibliotecaApp).printMessage(Mockito.anyString());
     }
+
+    @Test
+    public void checkIfMenuIsBeingShowed() throws Exception {
+
+        Mockito.doCallRealMethod().when(bibliotecaApp).continueToMenu(inputAsker);
+        bibliotecaApp.continueToMenu(inputAsker);
+        Mockito.verify(bibliotecaApp).showMenu();
+    }
+
 
 
 }
