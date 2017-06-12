3b012e90c19c7564386533b26a24f069ece1a369
mairaaraujo <maiira.araujo@gmail.com>
2017-06-12 00:59:01 -0300

    organizando design da aplicação

---
+++ src/main/java/com/twu/biblioteca/BibliotecaService.java
@@ -0,0 +1,17 @@
+package com.twu.biblioteca;
+
+
+import com.twu.inputAsker.InputAsker;
+
+public class BibliotecaService {
+
+    InputAsker inputAsker;
+
+    public BibliotecaService(InputAsker inputAsker) {
+        this.inputAsker = inputAsker;
+    }
+
+    public void printListBooks(InputAsker inputAsker) {
+
+    }
+}
--- src/test/java/com.twu/biblioteca/BibliotecaServiceTest.java
+++ src/test/java/com.twu/biblioteca/BibliotecaServiceTest.java
@@ -1,13 +1,27 @@
 package com.twu.biblioteca;
 
+import com.twu.inputAsker.InputAsker;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class BibliotecaServiceTest {
 
+    @Mock
+    InputAsker inputAsker;
+
+    @InjectMocks
+    BibliotecaService bibliotecaService;
+
     @Test
-    public void chooseValidMenuOption() throws Exception {
-        String userOption = "1";
-        
+    public void listBooksIsPrinted() throws Exception {
+
+        bibliotecaService = new BibliotecaService(inputAsker);
+        bibliotecaService.printListBooks(inputAsker);
+
 
     }
 }
